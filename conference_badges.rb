8095d0523bac1f07627e42a02d6f8bf99d007f37
Gigi M <mmgamao1@gmail.com>
2019-02-13 13:50:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,11 @@ name = "Arel"
 def badge_maker(name)
   puts "Hello, my name is #{name}"
 end
+
+def batch_badge_creator(name)
+  
+  
+end
+
+def assign_rooms(name, room_number)
+  puts "Hello, #{name}! You'll be assigned to room #{room_number}!"
